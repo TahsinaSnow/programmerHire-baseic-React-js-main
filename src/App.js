79a6e5c05283ed340c621155c40f970a006f2a0c
Tahsina Laboni <tahsinaa404@gmail.com>
2022-02-19 22:41:39 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/Header/Header";
+import Shop from "./components/Shop/Shop";
+
+
+function App() {
+  return (
+    <div className="App">
+        <Header></Header>
+      <Shop/>
+  
+
+    </div>
+  );
+}
+
+export default App;
